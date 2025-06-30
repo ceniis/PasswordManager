177d0a5a051d40ecb444d63de09e52fc9d146b7e
ceniis <ahtyrskaasofia@gmail.com>
2025-06-30 15:35:51 +0300

    Add Forgot the password option. Change interface

--- WinForm/PasswordManager_WinForms/LoginForm.Designer.cs
+++ WinForm/PasswordManager_WinForms/LoginForm.Designer.cs
@@ -31,45 +31,49 @@
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             label1 = new Label();
-            label2 = new Label();
+            InputTitleLabel = new Label();
             textBox1 = new TextBox();
             btnConfirm = new Button();
             btnHelp = new Button();
             toolTip1 = new ToolTip(components);
             btnClose = new Button();
             btnShow = new Button();
+            linkLabelForgotThePassword = new LinkLabel();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.FlatStyle = FlatStyle.Flat;
-            label1.Font = new Font("Corbel", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(6, 9);
+            label1.Font = new Font("Yu Gothic UI Semibold", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(140, 10);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(221, 49);
+            label1.Size = new Size(145, 41);
             label1.TabIndex = 0;
-            label1.Text = "WELCOME!";
+            label1.Text = "Welcome";
             label1.TextAlign = ContentAlignment.TopCenter;
             // 
-            // label2
+            // InputTitleLabel
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Corbel", 12F);
-            label2.Location = new Point(6, 75);
-            label2.Name = "label2";
-            label2.Size = new Size(234, 24);
-            label2.TabIndex = 1;
-            label2.Text = "Please, enter the password";
+            InputTitleLabel.AutoSize = true;
+            InputTitleLabel.Font = new Font("Yu Gothic UI", 11F);
+            InputTitleLabel.Location = new Point(8, 76);
+            InputTitleLabel.Margin = new Padding(4, 0, 4, 0);
+            InputTitleLabel.Name = "InputTitleLabel";
+            InputTitleLabel.Size = new Size(45, 25);
+            InputTitleLabel.TabIndex = 1;
+            InputTitleLabel.Text = "Text";
             // 
             // textBox1
             // 
             textBox1.Cursor = Cursors.IBeam;
-            textBox1.Font = new Font("Corbel Light", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Font = new Font("Yu Gothic UI", 11F);
             textBox1.ForeColor = SystemColors.GrayText;
-            textBox1.Location = new Point(6, 101);
+            textBox1.Location = new Point(8, 105);
+            textBox1.Margin = new Padding(4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(279, 26);
+            textBox1.Size = new Size(385, 32);
             textBox1.TabIndex = 2;
             textBox1.Text = "Password...";
             textBox1.Enter += textBox1_Enter;
@@ -85,11 +89,11 @@
             btnConfirm.FlatAppearance.BorderSize = 2;
             btnConfirm.FlatAppearance.MouseOverBackColor = Color.PaleTurquoise;
             btnConfirm.FlatStyle = FlatStyle.Flat;
-            btnConfirm.Font = new Font("Corbel", 12F);
-            btnConfirm.Location = new Point(199, 141);
+            btnConfirm.Font = new Font("Yu Gothic UI", 11F);
+            btnConfirm.Location = new Point(8, 145);
             btnConfirm.Margin = new Padding(0);
             btnConfirm.Name = "btnConfirm";
-            btnConfirm.Size = new Size(86, 39);
+            btnConfirm.Size = new Size(88, 38);
             btnConfirm.TabIndex = 3;
             btnConfirm.Text = "OK";
             btnConfirm.UseVisualStyleBackColor = false;
@@ -103,10 +107,10 @@
             btnHelp.FlatStyle = FlatStyle.Flat;
             btnHelp.Font = new Font("Segoe UI", 14F);
             btnHelp.ForeColor = Color.Navy;
-            btnHelp.Location = new Point(277, -1);
+            btnHelp.Location = new Point(346, -1);
             btnHelp.Margin = new Padding(0);
             btnHelp.Name = "btnHelp";
-            btnHelp.Size = new Size(45, 41);
+            btnHelp.Size = new Size(56, 51);
             btnHelp.TabIndex = 4;
             btnHelp.Text = "❔";
             btnHelp.UseVisualStyleBackColor = true;
@@ -120,10 +124,10 @@
             btnClose.FlatStyle = FlatStyle.Flat;
             btnClose.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnClose.ForeColor = Color.Black;
-            btnClose.Location = new Point(313, 0);
+            btnClose.Location = new Point(391, 0);
             btnClose.Margin = new Padding(0);
             btnClose.Name = "btnClose";
-            btnClose.Size = new Size(45, 41);
+            btnClose.Size = new Size(56, 51);
             btnClose.TabIndex = 5;
             btnClose.Text = "❌";
             btnClose.UseVisualStyleBackColor = true;
@@ -141,29 +145,44 @@
             btnShow.FlatStyle = FlatStyle.Flat;
             btnShow.Font = new Font("Corbel", 12F);
             btnShow.ForeColor = Color.Black;
-            btnShow.Location = new Point(289, 93);
+            btnShow.Location = new Point(391, 105);
             btnShow.Margin = new Padding(0);
             btnShow.Name = "btnShow";
-            btnShow.Size = new Size(24, 24);
+            btnShow.Size = new Size(46, 26);
             btnShow.TabIndex = 12;
             btnShow.UseVisualStyleBackColor = false;
             btnShow.Click += btnShow_Click;
             // 
+            // linkLabelForgotThePassword
+            // 
+            linkLabelForgotThePassword.AutoSize = true;
+            linkLabelForgotThePassword.Font = new Font("Yu Gothic UI", 11F);
+            linkLabelForgotThePassword.Location = new Point(216, 145);
+            linkLabelForgotThePassword.Name = "linkLabelForgotThePassword";
+            linkLabelForgotThePassword.Size = new Size(183, 25);
+            linkLabelForgotThePassword.TabIndex = 13;
+            linkLabelForgotThePassword.TabStop = true;
+            linkLabelForgotThePassword.Text = "Forgot the password";
+            linkLabelForgotThePassword.LinkClicked += linkLabelForgotThePassword_LinkClicked;
+            // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Gainsboro;
-            ClientSize = new Size(357, 189);
+            ClientSize = new Size(446, 194);
+            Controls.Add(linkLabelForgotThePassword);
             Controls.Add(btnShow);
             Controls.Add(btnClose);
             Controls.Add(btnHelp);
             Controls.Add(btnConfirm);
             Controls.Add(textBox1);
-            Controls.Add(label2);
+            Controls.Add(InputTitleLabel);
             Controls.Add(label1);
+            Font = new Font("Yu Gothic UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4);
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "LoginForm";
@@ -175,12 +194,13 @@
         #endregion
 
         private Label label1;
-        private Label label2;
+        private Label InputTitleLabel;
         private TextBox textBox1;
         private Button btnConfirm;
         private Button btnHelp;
         private ToolTip toolTip1;
         private Button btnClose;
         private Button btnShow;
+        private LinkLabel linkLabelForgotThePassword;
     }
 }
--- WinForm/PasswordManager_WinForms/LoginForm.cs
+++ WinForm/PasswordManager_WinForms/LoginForm.cs
@@ -4,20 +4,168 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Text.Json;
-using System.Linq.Expressions;
-using System.Security.Cryptography;
+using static PasswordManager_WinForms.FileManager;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace PasswordManager_WinForms
 {
     public partial class LoginForm : Form
     {
+        string enterPassword = "Please, enter the password";
+        string enterCodeWord = "Please, enter the code-word";
+        bool fileExists;
+        bool wordMode = false;
+        bool resetPasswordMode = false;
+
+        Dictionary<string, string> saveData = new Dictionary<string, string>();
+        string fileName = FilePaths.LoginPasswordFile; // file
+
         public LoginForm()
         {
             InitializeComponent();
+            fileExists = File.Exists(fileName);
+
+            if (!fileExists)
+            {
+                linkLabelForgotThePassword.Hide();
+            }
+
+            InputTitleLabel.Text = enterPassword;
+            textBox1.ForeColor = Color.Gray;
+            textBox1.PasswordChar = '\0';
+            textBox1.Text = "Password...";
+        }
+
+        private void Logination()
+        {
+            string jsonString = File.ReadAllText(fileName);
+            saveData = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString);
+
+            if (saveData == null || !saveData.ContainsKey("loginPassword") || !saveData.ContainsKey("codeWord"))
+            {
+                MessageBox.Show("Invalid file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                fileExists = false;
+                return;
+            }
+
+            if (!resetPasswordMode)
+            {
+                string storedHash = saveData["loginPassword"];
+                string hashString;
+
+                using (SHA256 sha = SHA256.Create())
+                {
+                    byte[] tmpHash = sha.ComputeHash(Encoding.UTF8.GetBytes(textBox1.Text));
+                    hashString = Convert.ToBase64String(tmpHash);
+                }
+
+                if (storedHash == hashString)
+                {
+                    this.DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect password. Try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else // reset the password
+            {
+                string hashString;
+
+                using (SHA256 sha = SHA256.Create())
+                {
+                    byte[] tmpHash = sha.ComputeHash(Encoding.UTF8.GetBytes(textBox1.Text));
+                    hashString = Convert.ToBase64String(tmpHash);
+                }
+
+                if (wordMode)
+                {
+                    string storedHash = saveData["codeWord"];
+                    if (storedHash == hashString)
+                    {
+                        wordMode = false;
+                        InputTitleLabel.Text = enterPassword;
+                        textBox1.Text = "Password...";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect code-word. Try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    DialogResult result = MessageBox.Show(
+                       "Keep new password as an entry password?",
+                       "Create a password",
+                       MessageBoxButtons.YesNo,
+                       MessageBoxIcon.Question
+                   );
+
+                    if (result == DialogResult.Yes)
+                    {
+                        saveData["loginPassword"] = hashString;
+                        jsonString = JsonSerializer.Serialize(saveData, new JsonSerializerOptions { WriteIndented = true });
+                        File.WriteAllText(fileName, jsonString);
+                        MessageBox.Show("Password was successfully updated", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.DialogResult = DialogResult.OK;
+                    }
+                }
+            }
+        }
+
+        private void Registration()
+        {
+            string hashString;
+
+            using (SHA256 sha = SHA256.Create())
+            {
+                byte[] tmpHash = sha.ComputeHash(Encoding.UTF8.GetBytes(textBox1.Text));
+                hashString = Convert.ToBase64String(tmpHash);
+            }
+
+            if (!wordMode)
+            {
+                DialogResult result = MessageBox.Show(
+                       "Keep current password as an entry password?",
+                       "Create a password",
+                       MessageBoxButtons.YesNo,
+                       MessageBoxIcon.Question
+                   );
+
+                if (result == DialogResult.Yes)
+                {
+                    saveData.Add("loginPassword", hashString);
+                    InputTitleLabel.Text = enterCodeWord;
+                    textBox1.Text = "Code-word...";
+                    wordMode = true;
+                }
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show(
+                      "Keep current code-word as an code-word for resetting password?",
+                      "Create a code-word",
+                      MessageBoxButtons.YesNo,
+                      MessageBoxIcon.Question
+                  );
+
+                if (result == DialogResult.Yes)
+                {
+                    saveData.Add("codeWord", hashString);
+                }
+
+                string jsonString = JsonSerializer.Serialize(saveData, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(fileName, jsonString);
+                MessageBox.Show("Login information is saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.DialogResult = DialogResult.OK;
+            }
         }
 
         /// <summary>
@@ -27,62 +175,13 @@ namespace PasswordManager_WinForms
         {
             try
             {
-                string fileName = FilePaths.LoginPasswordFile; // file
-                string password = textBox1.Text; // login password
-
-                // hash the input
-                string hashString;
-                using (SHA256 sha = SHA256.Create())
+                if (fileExists)
                 {
-                    byte[] tmpHash = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
-                    hashString = Convert.ToBase64String(tmpHash);
+                    Logination();
                 }
-
-                if (File.Exists(fileName))
+                else
                 {
-                    string jsonString = File.ReadAllText(fileName);
-                    var jsonData = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString);
-
-                    if (jsonData == null || !jsonData.ContainsKey("loginPassword"))
-                    {
-                        MessageBox.Show("Invalid password file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-
-                    string storedHash = jsonData["loginPassword"];
-
-                    // compare the hash
-                    if (storedHash == hashString)
-                    {
-                        this.DialogResult = DialogResult.OK;
-                    }
-                    else
-                    {
-                        MessageBox.Show("Incorrect password. Try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    }
-                }
-                else // if file doesn't exist
-                {
-                    DialogResult result = MessageBox.Show(
-                        "Keep current password as an entry password?",
-                        "Create a password",
-                        MessageBoxButtons.YesNo,
-                        MessageBoxIcon.Question
-                    );
-
-                    if (result == DialogResult.Yes)
-                    {
-                        var data = new Dictionary<string, string> { { "loginPassword", hashString } };
-                        string jsonString = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
-                        File.WriteAllText(fileName, jsonString);
-
-                        this.DialogResult = DialogResult.OK;
-                        MessageBox.Show("Password is written successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                    else
-                    {
-                        textBox1.Focus();
-                    }
+                    Registration();
                 }
             }
             catch (Exception ex)
@@ -119,28 +218,41 @@ namespace PasswordManager_WinForms
 
         private void textBox1_Enter(object sender, EventArgs e)
         {
-            if (textBox1.Text == "Password...")
+            if (textBox1.Text == "Password..." || textBox1.Text == "Code-word...")
             {
                 textBox1.Text = "";
                 textBox1.ForeColor = Color.Black;
+                textBox1.PasswordChar = '●';
             }
         }
 
         private void textBox1_Leave(object sender, EventArgs e)
         {
-            if (textBox1.Text == string.Empty)
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
             {
-                textBox1.Text = "Password...";
                 textBox1.ForeColor = Color.Gray;
+                textBox1.PasswordChar = '\0';
+                textBox1.Text = wordMode ? "Code-word..." : "Password...";
             }
         }
 
         private void LoginForm_Load(object sender, EventArgs e)
         {
-            // toolTips settings
             toolTip1.SetToolTip(btnShow, "Show the password");
             toolTip1.SetToolTip(btnHelp, "Information");
             toolTip1.SetToolTip(btnConfirm, "Confirm");
+        }
+
+        private void linkLabelForgotThePassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabelForgotThePassword.Hide();
+            resetPasswordMode = true;
+            wordMode = true;
+            MessageBox.Show("Enter the code-word to confirm it's you", "Reset the password", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            InputTitleLabel.Text = enterCodeWord;
+            textBox1.Text = "Code-word...";
+            textBox1.ForeColor = Color.Gray;
+            InputTitleLabel.Focus();
         }
     }
 }
